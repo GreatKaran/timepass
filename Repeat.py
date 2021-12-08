406673398597895ee1be86ac4b2d2851fd08ae22
GreatKaran <86833061+GreatKaran@users.noreply.github.com>
2021-12-08 19:24:31 +0530

    Update Repeat.py

--- Repeat.py
+++ Repeat.py
@@ -1,8 +1,7 @@
 import time
 import webbrowser 
 link = input("Link de BSDK")
-sleep = input(int("kitna ruku"))
 while(0 == 0):
   print("made by @phoenixop7")
   webbrowser.open(link)
-  time.sleep(sleep)
+  time.sleep(10)
