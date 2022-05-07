d58f5b65fb204d490d0ed4b48d101c1c65f05d64
GreatKaran <86833061+GreatKaran@users.noreply.github.com>
2022-05-07 15:06:59 +0530

    Delete Repeat.py

--- Repeat.py
+++
@@ -1,9 +0,0 @@
-import time
-import webbrowser
-import os
-link = input("Link de BSDK")
-while(0 == 0):
- webbrowser.open(link , new=2)
- print("made by @phoenixop7")
- time.sleep(20)
- os.system("taskkill /im chrome.exe /f")
